87fb2180175a390bdf662b54c49027e2a5650497
Jason Traub <jtraub91@gmail.com>
2024-12-26 14:54:15 -0600

    add some initial style, layout, and images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header class="absolute w-full flex flex-row">
+  <div class="top-0 left-0 h-20 w-20">
+    <img src="/images/yoshi-github-jtraub91.jpg"/>
+  </div>
+  <a href="#" class="mx-4 mt-auto mb-0 font-mono text-blue-500 hover:text-green-400 underline text-4xl">Blog</a>
+  <a href="#" class="mx-4 mt-auto mb-0 font-mono text-blue-500 hover:text-green-400 text-lg flex">
+    <span class="mb-0 mt-auto mx-auto">
+      More Links
+    </span>
+  </a>
+  <a href="mailto:jtraub91@gmail.com" class="ml-auto mr-4 mt-auto mb-0 font-mono text-blue-500 hover:text-green-400 text-4xl">
+    <span class="">Contact</span>
+    <span>🤙</span>
+  </a>
+</header>
