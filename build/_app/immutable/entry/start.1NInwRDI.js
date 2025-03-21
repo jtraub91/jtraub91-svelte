fd44ad969a4ff98d7b6f3ba1831e88aa6a079e95
Jason Traub <jtraub91@gmail.com>
2025-03-21 15:26:12 -0500

    change base path to ''

--- build/_app/immutable/entry/start.1NInwRDI.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DNYI3SCP.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 		},
 		adapter: adapter(),
 		paths: {
-			base: '/jtraub91-svelte',
+			base: '',
 		},
 	},
 };
